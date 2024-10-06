3ac8aef7ca194065dc1c51a6bdb3ea7465c1af5c
Basha575 <arebasha4@gmail.com>
2024-10-06 19:28:37 +0530

    Add files via upload

---
+++ netro.sh
@@ -0,0 +1,6 @@
+# Enc @NetroVip
+#By NetroYT
+
+z="
+";dz='etro';gz='>> "';az='arti';Jz='_mes';mz='ue; ';Zz=' "St';Tz=' "$L';bz='ng t';Gz='func';fz='.." ';Qz=' $(d';Bz='ILE=';hz='$LOG';Wz='}';Lz='() {';ez='Vip.';Nz=' "Ne';oz='log_';Yz='ILE"';Xz='> "$';Ez='her.';Iz=' log';jz='"';qz='age';Cz='Netr';cz='he N';Vz='LE"';kz='whil';lz='e tr';nz='do';Dz='oFat';Sz='" >>';sz='p 60';Az='LOGF';Pz='King';tz='done';iz='FILE';Kz='sage';Mz='echo';rz='slee';Fz='log';Hz='tion';Oz='tro ';Uz='OGFI';pz='mess';Rz='ate)';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$z$Wz$z$Xz$Az$Yz$z$Mz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$z$kz$lz$mz$nz$z$oz$pz$qz$z$rz$sz$z$tz"
